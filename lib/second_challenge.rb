d58c3e40610e36abc346c163303855e1045f7f82
shadowfox3735 <normanpan180@gmail.com>
2018-11-15 15:44:23 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,8 +7,10 @@ def second_challenge
       :hero_friends = []
       }, 
     :capulet => {
-      :
-      }}
-
-  
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends = []
+      }
+  }
 end
