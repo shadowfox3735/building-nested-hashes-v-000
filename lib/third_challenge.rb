b16bfb26305364a2951ee1dbd8932a049bce30a5
shadowfox3735 <normanpan180@gmail.com>
2018-11-15 15:51:30 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,22 +11,24 @@ def third_challenge
         },
       :hero => {
         :name => "Romeo",
-        :age => "54",
+        :age => "15",
         :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        :name => "Lord Capulet"
+        :name => "Lord Capulet",
         :age => "50"
         },
       :matriarch => {
-        :name => "Lady Capulet"
-        :age =>
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
